3e33c41e82a6eb47922f196810d4ebca726964c3
Joaquin Noe <joako.n96@gmail.com>
2023-03-30 17:50:20 -0300

    FEAT (wallet-be) Add calls to etherscan api

---
+++ wallet-be/wallet/src/infrastructure/api/etherscan.spec.ts
@@ -0,0 +1,79 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { Etherscan } from './etherscan';
+
+describe('Etherscan', () => {
+  let etherscan: Etherscan;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        Etherscan,
+        { provide: 'ETH_API_KEY', useValue: 'your-api-key-here' },
+      ],
+    }).compile();
+
+    etherscan = module.get<Etherscan>(Etherscan);
+  });
+
+  describe('getBalance', () => {
+    it('should return the balance for a valid Ethereum address', async () => {
+      const address = '0x1234567890123456789012345678901234567890';
+      global.fetch = jest.fn(() =>
+        Promise.resolve({
+          json: () =>
+            Promise.resolve({
+              status: '1',
+              message: 'OK',
+              result: '864929740948334',
+            }),
+        }),
+      ) as jest.Mock;
+      const balance = await etherscan.getBalance(address);
+      expect(balance).toBeGreaterThan(0);
+    });
+    it('throws an error if the Ethereum address is not valid', async () => {
+      const invalidAddress = '0xinvalidaddress';
+      await expect(etherscan.getBalance(invalidAddress)).rejects.toThrow(
+        'Invalid address',
+      );
+    });
+
+    it('throws an error if the API returns an error message', async () => {
+      const address = '0x1234567890123456789012345678901234567890';
+      global.fetch = jest.fn(() =>
+        Promise.resolve({
+          json: () =>
+            Promise.resolve({ status: '0', message: 'Invalid API Key' }),
+        }),
+      ) as jest.Mock;
+      await expect(etherscan.getBalance(address)).rejects.toThrow(
+        'Invalid API Key',
+      );
+    });
+  });
+
+  describe('getTransactions', () => {
+    it('throws an error if the Ethereum address is not valid', async () => {
+      const invalidAddress = '0xinvalidaddress';
+      await expect(etherscan.getTransactions(invalidAddress)).rejects.toThrow(
+        'Invalid address',
+      );
+    });
+
+    it('throws an error if the API returns an error message', async () => {
+      const address = '0x1234567890123456789012345678901234567890';
+      global.fetch = jest.fn(() =>
+        Promise.resolve({
+          json: () =>
+            Promise.resolve({
+              status: '0',
+              message: 'Invalid API Key',
+            }),
+        }),
+      ) as jest.Mock;
+      await expect(etherscan.getTransactions(address)).rejects.toThrow(
+        'Invalid API Key',
+      );
+    });
+  });
+});
