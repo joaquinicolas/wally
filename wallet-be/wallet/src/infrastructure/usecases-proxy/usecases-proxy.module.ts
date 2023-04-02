6b08bd9ab050bdc2a15520d15c10b9bbd8f25948
Joaquin Noe <joako.n96@gmail.com>
2023-04-01 21:59:59 -0300

    FEAT (usecases) Add getWallet and getExhangeRates usecases

---
+++ wallet-be/wallet/src/infrastructure/usecases-proxy/usecases-proxy.module.ts
@@ -0,0 +1,34 @@
+import { DynamicModule, Module } from '@nestjs/common';
+import { EthAPI } from 'src/domain/ethRepository.interface';
+import { UseCaseProxy } from './usecases-proxy';
+import { Etherscan } from '../api/etherscan';
+import { GetWalletUseCase } from 'src/usecases/getWallet.usecases';
+import { GetExchangeRatesUseCase } from 'src/usecases/getExchange.usecases';
+
+@Module({
+  imports: [],
+})
+export class UseCasesProxyModule {
+  static GET_WALLET_USECASES_PROXY = 'getWalletUsecasesProxy';
+  static GET_EXCHANGE_RATE_USECASES_PROXY = 'getExchangeRateUsecasesProxy';
+
+  static register(): DynamicModule {
+    return {
+      module: UseCasesProxyModule,
+      providers: [
+        {
+          inject: [Etherscan],
+          provide: UseCasesProxyModule.GET_WALLET_USECASES_PROXY,
+          useFactory: (externalAPI: EthAPI) =>
+            new UseCaseProxy(new GetWalletUseCase(externalAPI)),
+        },
+        {
+          provide: UseCasesProxyModule.GET_EXCHANGE_RATE_USECASES_PROXY,
+          useFactory: () => {
+            return new UseCaseProxy(new GetExchangeRatesUseCase());
+          },
+        },
+      ],
+    };
+  }
+}
