6898646eef0af103ad06b2f3d3b3965a4cc8baa8
Joaquin Noe <joako.n96@gmail.com>
2023-04-04 12:42:32 -0300

    FEAT (exchange) Add controller

--- wallet-be/wallet/src/infrastructure/controllers/wallet/wallet.controller.ts
+++ wallet-be/wallet/src/infrastructure/controllers/wallet/wallet.controller.ts
@@ -11,6 +11,8 @@ import { UseCaseProxy } from 'src/infrastructure/usecases-proxy/usecases-proxy';
 import { UseCasesProxyModule } from 'src/infrastructure/usecases-proxy/usecases-proxy.module';
 import { GetWalletUseCase } from 'src/usecases/getWallet.usecases';
 import { ApiResponse } from '@nestjs/swagger';
+import { Rate } from 'src/domain/models/rate';
+import { GetExchangeRatesUseCase } from 'src/usecases/getExchange.usecases';
 @Controller('wallet')
 @ApiResponse({
   status: 500,
@@ -20,6 +22,8 @@ export class WalletController {
   constructor(
     @Inject(UseCasesProxyModule.GET_WALLET_USECASES_PROXY)
     private readonly getWalletUsecasesProxy: UseCaseProxy<GetWalletUseCase>,
+    @Inject(UseCasesProxyModule.GET_EXCHANGE_RATE_USECASES_PROXY)
+    private readonly getExchangeRateUsecasesProxy: UseCaseProxy<GetExchangeRatesUseCase>,
   ) {}
 
   @Get(':address')
@@ -29,6 +33,19 @@ export class WalletController {
         .getInstance()
         .execute(address);
       return wallet;
+    } catch (err) {
+      throw new HttpException(err.message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  @Get('exchange')
+  async exchange(): Promise<Rate[]> {
+    try {
+      const rates = await this.getExchangeRateUsecasesProxy
+        .getInstance()
+        .execute();
+
+      return rates;
     } catch (err) {
       throw new HttpException(err.message, HttpStatus.INTERNAL_SERVER_ERROR);
     }
